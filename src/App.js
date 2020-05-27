d40fa80297e88e08d88e1c14b1f8880ebee5fc8d
kamarabbas99 <41794314+kamarabbas99@users.noreply.github.com>
2020-05-27 18:41:05 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,61 @@ import Navigation from'./components/Navigation/Navigation';
 import './App.css';
 import Logo from'./components/Logo/Logo';
 import ImageLinkForm from'./components/ImageLinkForm/ImageLinkForm';
+import Rank from'./components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from'clarifai';
+
+const particlesOptions={
+particles: {
+ number: {
+  value:90,
+  density: {
+    enable: true,
+    value_area: 800
+  }
+ }
+}
+
+}
+
+const app = new Clarifai.App({
+ apiKey: '7294288680b04e88b6ac0f54a76d863c'
+});
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+       input:'',
+    }
+  }
+  OnInputChange = (event)=> {
+    console.log(event.target.value);
+    
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+     console.log(response); 
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
   render(){
    return (
     <div className="App">
+      <Particles className='particles' 
+        params={particlesOptions} 
+      />
       <Navigation/>
       <Logo/>
-      <ImageLinkForm/>
+      <Rank/>
+      <ImageLinkForm OnInputChange={this.OnInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      
     {/*
 
       <FaceRecognition/>*/}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
-const ImageLinkForm =() =>{
+import'./ImageLinkForm.css';
+const ImageLinkForm =({ OnInputChange , onButtonSubmit}) =>{
  return (
    <div>
    	<p className='f3'>{'This Magic Brain will detect faces in your pictures'}</p>
    	<div className='center'>
-   		<input className='f4 pa2 w-70 center'type='text'/>
-   		<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Delete</button>
+   	 <div className='form center pa4 br3 shadow-5'>
+   		<input className='f4 pa2 w-70 center'type='text' onChange={OnInputChange}/>
+   		<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Delete</button>
+   	 </div>
    	</div>
    </div>
  );
