c2a38d94c8df6c400ecfc93ae522855628bcf90c
unknown <saiyedkamarabbas64@gmail.com>
2020-06-06 20:02:18 -0500

    Updates

---
+++ precache-manifest.2cc097a9b5f50e446fbadaf5972351bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaee4bc2f6f31c0c0c643ffa6d623175",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d5f1df5164668a903d7f",
+    "url": "/face-recognition/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/face-recognition/static/js/2.61848096.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.61848096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f1df5164668a903d7f",
+    "url": "/face-recognition/static/js/main.c11232d5.chunk.js"
+  },
+  {
+    "revision": "175c389479d1898dca91",
+    "url": "/face-recognition/static/js/runtime-main.dd9770d3.js"
+  }
+]);
